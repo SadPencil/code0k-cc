a112e90d1b3ea8ba6f3f13c38565331d9258c5b7
Sad Pencil <qh06@qq.com>
2020-05-22 03:55:40 +0800

    update example

---
+++ Examples/Power.c0k.c
@@ -0,0 +1,14 @@
+uint32 MAX_LOOP=100;
+
+input uint32 a;
+nizkinput uint32 b;
+output uint32 c;
+
+void main() max (1){
+    c = 1;
+    uint32 i = 0;
+    while (i < b) max (MAX_LOOP){
+    	c = c * a;
+        i = i + 1;
+    }
+}
